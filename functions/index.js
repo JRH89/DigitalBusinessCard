ca4b81d43fa31655d24a838b076997ad0bc01575
JRH89 <thegemlins@gmail.com>
2023-03-01 02:58:45 -0800

    ok

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,19 @@
-const fs = require('fs');
+import { writeFileSync } from 'fs';
+import { join } from 'path';
 
-exports.handler = async function(event, context) {
+export async function handler(event, context) {
   // Parse the POST body from the request
   const body = JSON.parse(event.body);
 
+  // Set the path to the profile.json file in the root directory
+  const filePath = join(__dirname, '../profile.json');
+
   // Write the body to a JSON file
-  fs.writeFileSync('./profile.json', JSON.stringify(body));
+  writeFileSync(filePath, JSON.stringify(body));
 
   // Return a success response
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Profile saved successfully' })
+    body: JSON.stringify({ message: 'Data saved successfully' })
   };
-};
+}
