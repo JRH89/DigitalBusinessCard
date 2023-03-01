d690c8046fa697de479de4aa9403e5bc31a8267f
JRH89 <thegemlins@gmail.com>
2023-03-01 01:59:02 -0800

    f

--- script2.js
+++ script2.js
@@ -1,20 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Get the query parameters from the URL
-    const urlSearchParams = new URLSearchParams(window.location.search);
-    const params = Object.fromEntries(urlSearchParams.entries());
   
-    fetch('/profile.json')
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById('name2').innerText = data.name || '';
-    document.getElementById('job2').innerText = data.job || '';
-    document.getElementById('l1').href = data.link1 || '';
-    document.getElementById('l2').href = data.link2 || '';
-    document.getElementById('l3').href = data.link3 || '';
-    document.getElementById('l4').href = data.link4 || '';
-    document.getElementById('l5').href = data.link5 || '';
-    document.getElementById('l6').href = data.link6 || '';
-  })
-  });
 
-  
+  fetch('/profile.json')
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('name2').innerText = data.name || '';
+      document.getElementById('job2').innerText = data.job || '';
+      document.getElementById('l1').href = data.link1 || '';
+      document.getElementById('l2').href = data.link2 || '';
+      document.getElementById('l3').href = data.link3 || '';
+      document.getElementById('l4').href = data.link4 || '';
+      document.getElementById('l5').href = data.link5 || '';
+      document.getElementById('l6').href = data.link6 || '';
+      
+      // Redirect the user to the profile page
+      window.location.href = 'profile.html';
+    });
+});
