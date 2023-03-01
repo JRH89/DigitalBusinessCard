e58a2eaf14460463589f4c48e2cd96a8b45b8f00
JRH89 <thegemlins@gmail.com>
2023-03-01 15:41:57 -0800

    ok

--- script.js
+++ script.js
@@ -1,47 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // Get the form element
-  var form = document.getElementById("my-form");
+document.getElementById("my-form").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent default form submission behavior
+  
+  // Get form input values
+  var name = document.getElementById("name2").value;
+  var job = document.getElementById("job2").value;
+  var website = document.getElementById("link1").value;
+  var github = document.getElementById("link2").value;
+  var linkedin = document.getElementById("link3").value;
+  var twitter = document.getElementById("link4").value;
+  var reddit = document.getElementById("link5").value;
+  var email = document.getElementById("link6").value;
 
-  // Add a submit event listener to the form
-  form.addEventListener("submit", function(event) {
-    // Prevent the form from submitting normally
-    event.preventDefault();
+  // Create JSON object
+  var data = {
+    "name": name,
+    "job": job,
+    "website": website,
+    "github": github,
+    "linkedin": linkedin,
+    "twitter": twitter,
+    "reddit": reddit,
+    "email": email
+  };
 
-    // Get the user input
-    var name1 = document.getElementById("name2").value;
-    var job1 = document.getElementById("job2").value;
-    var link1 = document.getElementById("link1").value;
-    var link2 = document.getElementById("link2").value;
-    var link3 = document.getElementById("link3").value;
-    var link4 = document.getElementById("link4").value;
-    var link5 = document.getElementById("link5").value;
-    var link6 = document.getElementById("link6").value;
-
-    // Construct the form data object
-    var data = {
-      name: name1,
-      job: job1,
-      link1: link1,
-      link2: link2,
-      link3: link3,
-      link4: link4,
-      link5: link5,
-      link6: link6
-    };
-
-    // Send a POST request to the Netlify function
-    fetch("/.netlify/functions/index", {
-
-      method: "POST",
-      body: JSON.stringify(data)
-    })
-      .then(function(response) {
-        // Handle the response
-        console.log("Data updated successfully.");
-      })
-      .catch(function(error) {
-        // Handle the error
-        console.error(error);
-      });
-  });
+  // Convert JSON object to string
+  var jsonData = JSON.stringify(data);
+  
+  // Save JSON data to localStorage
+  localStorage.setItem("profileData", jsonData);
 });
