efd9a0b69e7c5b58266a86b5b5f614405dedb398
JRH89 <thegemlins@gmail.com>
2023-03-12 20:10:00 -0700

    ok

--- functions/signup.js
+++ functions/signup.js
@@ -6,28 +6,25 @@ const connection = mysql.createConnection({
   password: 'GratefulDead33!',
   database: 'dbkusers'
 });
+
 exports.handler = function(event, context, callback) {
-    const body = JSON.parse(event.body);
-    const username = body.username;
-    const email = body.email;
-    const password = body.password;
+  const body = JSON.parse(event.body);
+  const username = body.username;
+  const email = body.email;
+  const password = body.password;
   
-    // Insert the user into the database
-    const sql = `INSERT INTO users (username, email, password) VALUES (?, ?, ?)`;
-    connection.query(sql, [username, email, password], function(error, results, fields) {
-      if (error) {
-        console.log(error);
-        callback(error);
-      } else {
-        console.log(results);
-        callback(null, {
-          statusCode: 200,
-          body: JSON.stringify({ message: 'User added successfully' })
-        });
-      }
-    });
-  };
-
-  module.exports = signup;
-
-  
+  // Insert the user into the database
+  const sql = `INSERT INTO users (username, email, password) VALUES (?, ?, ?)`;
+  connection.query(sql, [username, email, password], function(error, results, fields) {
+    if (error) {
+      console.log(error);
+      callback(error);
+    } else {
+      console.log(results);
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'User added successfully' })
+      });
+    }
+  });
+};
